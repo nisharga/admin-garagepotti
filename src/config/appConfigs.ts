e8df2fc3846e6ae35c90cfbb931fe642469033c3
nisharga <kabirnisharga@gmail.com>
2025-03-01 19:49:58 +0600

    feat: middleware-removed

--- src/config/appConfigs.ts
+++ src/config/appConfigs.ts
@@ -1,9 +1,8 @@
 import { Outfit } from 'next/font/google';
 
-export const SITE_TITLE_DEFAULT =
-    'garagepotti | A Smart Solutions for covert money';
-export const SITE_TITLE_TEMPLATE_DEFAULT = `%s - garagepotti`;
-export const SITE_DESCRIPTION_DEFAULT = 'garagepotti Description';
+export const SITE_TITLE_DEFAULT = 'Garagepotti | A Smart Solutions for Cars';
+export const SITE_TITLE_TEMPLATE_DEFAULT = `%s - Garagepotti`;
+export const SITE_DESCRIPTION_DEFAULT = 'Ultimate solution for car owner';
 export const SITE_VERIFICATION_GOOGLE_DEFAULT =
     'google-site-verification=adwdawdaw';
 
